2e26d21b71b3a3c65b6c8ce7350b1187090ab9be
Karoliny Rufino <karolinysantosofc@outlook.com>
2024-04-15 18:43:52 -0300

    feat: creating about page, contact and adding projects

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,45 @@
-const toggleTheme = document.getElementById('toggleTheme');
-const rootHTML = document.documentElement;
+changeTheme();
+headerMenu();
+accordion();
 
-function changeTheme(){
-    const currentTheme = rootHTML.getAttribute("data-theme");
-
-    currentTheme === "dark" ? rootHTML.setAttribute("data-theme", "light") : rootHTML.setAttribute("data-theme", "dark");
-
+function changeTheme () {
+    const toggleTheme = document.getElementById('toggleTheme');
+    const rootHTML = document.documentElement;
     
-    toggleTheme.classList.toggle("bi-sun");
-    toggleTheme.classList.toggle("bi-moon-stars");
+    function changeTheme(){
+        const currentTheme = rootHTML.getAttribute("data-theme");
+    
+        currentTheme === "dark" ? rootHTML.setAttribute("data-theme", "light") : rootHTML.setAttribute("data-theme", "dark");
+    
+        
+        toggleTheme.classList.toggle("bi-sun");
+        toggleTheme.classList.toggle("bi-moon-stars");
+    }
+    
+    toggleTheme.addEventListener('click', changeTheme);
 }
 
-toggleTheme.addEventListener('click', changeTheme);
+function accordion() {
+    const accordionHeaders = document.querySelectorAll('.accordion--header');
+
+    accordionHeaders.forEach( header => {
+        header.addEventListener("click", () => {
+            const accordionItem = header.parentElement;
+            const accordionActive = accordionItem.classList.contains("active");
+
+            accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+        });
+    });
+
+}
+
+function headerMenu(){
+    const menuLinks = document.querySelectorAll('.menu-link');
+
+    menuLinks.forEach( item => {
+        item.addEventListener("click", ()=> {
+            menuLinks.forEach( i => i.classList.remove("active"));
+            item.classList.add("active");
+        });
+    });
+}
